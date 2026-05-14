dfc700687dea0c1df216d63e4acfba1e6204d41f
Uri Herrera <hello@uriherrera.com>
2026-05-14 16:16:47 -0600

    cleaunp up files from the source repo

--- build.7/libs/audio/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/camiloh/maui-core/libs/audio/src
-# Build directory: /home/camiloh/maui-core/build.7/libs/audio/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build.7/libs/power/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/camiloh/maui-core/libs/power/src
-# Build directory: /home/camiloh/maui-core/build.7/libs/power/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
